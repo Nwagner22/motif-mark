971df794808bdffc5b89750b6bacabaa1f5667e4
Nwagner22 <nwagner2@uoregon.edu>
2020-02-17 21:22:39 -0800

    added helpers file to contain helper functions

---
+++ Scripts/helpers.py
@@ -0,0 +1,15 @@
+"""
+Nick Wagner
+2/17/20
+
+File to hold all of the helper functions that assist with parsing 
+a fasta file and locating exons and motifs
+"""
+
+
+
+
+
+
+def first_func():
+    pass
