5206333019d53b0a22f4b7defc009c8ec9e63dd8
Nwagner22 <nwagner2@uoregon.edu>
2020-02-18 19:49:10 -0800

    added functions to find motifs and exons

--- Scripts/helpers.py
+++ Scripts/helpers.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
 """
 Nick Wagner
 2/17/20
@@ -11,5 +15,33 @@ a fasta file and locating exons and motifs
 
 
 
-def first_func():
-    pass
+def find_motifs(sequence, motif_seq):
+    list_of_motifs = []
+
+    motif_length = len(motif_seq)
+    count = 0
+    for i in range(len(sequence) - motif_length + 1):
+        current_slice = sequence[i:i+motif_length]
+        if current_slice == motif_seq:
+            current_coordinates = (i,i + motif_length - 1)
+            list_of_motifs.append(current_coordinates)
+            count += 1
+            
+
+    return list_of_motifs, count
+
+
+
+def find_exons(sequence):
+    list_of_exon_indexes = []
+    count = 0
+
+    for i in range(len(sequence)):
+        if sequence[i].isupper() == True:
+            list_of_exon_indexes.append(i)
+
+    return list_of_exon_indexes
+
+
+
+    
